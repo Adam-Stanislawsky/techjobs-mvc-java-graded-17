09964a4a3be1b9e72e3928cfe5d3f1a0ca628c80
adamstanislawsky <adamstanislawsky@gmail.com>
2024-11-09 00:39:01 -0600

    completed task 4, was using wrong return in displaySearchResults

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -41,9 +41,9 @@ public class SearchController {
             model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
         }
         model.addAttribute("jobs", jobs);
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
 
-        return "search/results";
+        return "list-jobs";
 
     }
 }
